14439a03b03e824891601b2227a92959fcdcd71b
Aljaž Ferenc <aljaz.ferenc@protonmail.com>
2024-04-10 03:34:01 +0200

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/logo.svg" type="image/x-icon" />
+      </head>
       <ContextProvider>
       <body  className={`${inter.className} text-white relative min-h-screen bg-black text-white`}>
         {children}
